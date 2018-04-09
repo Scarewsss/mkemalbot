1b1dde776a75892aa31581ea6e50e96df2ab9ecc
hzsagkol <37846356+hzsagkol@users.noreply.github.com>
2018-04-09 16:45:30 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message, params) => {
   .setColor(0x00ffff)
   .addField("**Eğlence ve Kullanıcı Komutları:**", `\nmkemal!herkesebendençay = Herkese Çay Alırsınız. \nmkemal!koş = Koşarsınız.\nmkemal!çayiç = Çay İçersiniz. \nmkemal!çekiç = İstediğiniz Kişiye Çekiç Atarsınız.\nmkemal!yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \nmkemal!sunucuresmi = BOT Sunucunun Resmini Atar. \nmkemal!sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \nmkemal!kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. \nmkemal!atam = Atamızın Fotoğrafını atar. \nmkemal!pasam = Atamızın Gifini atar. \nmkemal!bayrak = ŞANLI TÜRK BAYRAĞIMIZI GÖSTERİRİ `)
   .addField("**Sunucu Yetkilisi Komutları**", `mkemal!ban = İstediğiniz Kişiyi Sunucudan Banlar. \nmkemal!kick  = İstediğiniz Kişiyi Sunucudan Atar. \nmkemal!unban = İstediğiniz Kişinin Yasağını Açar. \nmkemal!sustur = İstediğiniz Kişiyi Susturur. -MUTED-`)
-  .addField("**Botun Ana Komutları**", "-yardım = BOT Komutlarını Atar. \nmkemal!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nmkemal!ping = BOT Gecikme Süresini Söyler. \nmkemal!davet = BOT Davet Linkini Atar. \nmkemal!istatistik = BOT İstatistiklerini Atar.")
+  .addField("**Botun Ana Komutları**", "mkemal!yardım = BOT Komutlarını Atar. \nmkemal!bilgi = BOT Kendisi Hakkında Bilgi Verir. \nmkemal!ping = BOT Gecikme Süresini Söyler. \nmkemal!davet = BOT Davet Linkini Atar. \nmkemal!istatistik = BOT İstatistiklerini Atar.")
   
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
