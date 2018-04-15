c76f6f25525ea13e87c12307362eb586916d0ad1
hzsagkol <37846356+hzsagkol@users.noreply.github.com>
2018-04-15 10:20:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-    msg.reply('Aleyküm selam,  hoş geldin ^^');
+    msg.reply('__AleykümSelam,Hoşgeldin mkemal!yardım yazarak komutlarıma ulaşabilirsin__ ^^');
   }
 });
 
